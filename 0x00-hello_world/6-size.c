87c4100c172f148cfb045e88abba1fb97a96bd09
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-17 14:48:36 -0700

    0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ float floatType;
 double doubleType;
 char charType;
 double int_Max;
-printf("Size of a char: %zu byte(s)", sizeof(charType));
-printf("Size of a int: %zu byte(s)", sizeof(intType));
-printf("Size of a long int: %zu byte(s)", sizeof(int_Max));
-printf("Size of a long long int: %zu byte(s)", sizeof(int_Max));
-printf("Size of a float: %zu byte(s)", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(int_Max));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(int_Max));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
