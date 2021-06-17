7bb1b337873a8b97d1a218df942e42467ccf9b06
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-17 14:42:16 -0700

    0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
 /**
  *main - prints datatype sizes
  *Description: prints a string
  *Return: 0
  */
-int main(void)
+int main(int argc, char** argv)
 {
 int intType;
 float floatType;
 double doubleType;
 char charType;
- 
-printf(sizeof(intType))
-printf(sizeof(floatType))
-printf(sizeof(doubleType))
-printf(sizeof(charType))
+printf("Size of a char: %zu byte(s)", sizeof(charType));
+printf("Size of a int: %zu byte(s)", sizeof(intType));
+printf("Size of a long int: %zu byte(s)", int_Max);
+printf("Size of a long long int: %zu byte(s)", int_Max);
+printf("Size of a float: %zu byte(s)", sizeof(floatType));
 return (0);
 }
