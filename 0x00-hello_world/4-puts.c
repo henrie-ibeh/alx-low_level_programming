6dd208c3f3d0c3072b8220390b92ceb2affa0c2d
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-17 12:44:50 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-  char words = "Programming is like building a multilingual puzzle\n";
+  char words[] = "Programming is like building a multilingual puzzle";
   puts(words); 
   return 0;
 }
