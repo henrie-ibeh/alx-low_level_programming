2e9d3e31210cf870418b3603342b77ad58d2fdc4
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-17 14:56:06 -0700

    0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  *Description: prints a string
  *Return: 0
  */
-int main(int argc, char** argv)
+int main(void)
 {
 int intType;
 float floatType;
@@ -16,7 +16,7 @@ char charType;
 double int_Max;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(intType));
+ printf("Size of a long int: %zu byte(s)\n", sizeof(int_Max));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(int_Max));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
