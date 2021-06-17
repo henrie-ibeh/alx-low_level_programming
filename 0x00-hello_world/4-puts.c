ddec2799997faa0cb587fd7a52a05b803ce472de
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-17 13:06:29 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
  *main - prints string
  *Description: prints a string
  *Return: 0
  */
+int main(void)
 {
 char words[] = "\"Programming is like building a multilingual puzzle";
 puts(words);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
  *main - prints string
- *Description: prints a string 
+ *Description: prints a string
  *Return: 0
  */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
