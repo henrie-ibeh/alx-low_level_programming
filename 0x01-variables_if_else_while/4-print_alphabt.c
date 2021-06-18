7fe70ce98ebafde87763fa8b4b8fa98a6d72a1e7
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-18 12:32:57 -0700

    7

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ *main - prints string
+ *Description: prints a string
+ *Return: 0
+ */
+int main(void)
+{
+char z[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+int len = strlen(z);
+for (i = 0; i < len; i++){
+if (z[i] == 'e')
+{
+i += 1;
+continue;
+}
+else if (z[i] == 'q')
+{
+i += 1;
+continue;
+}
+else
+{
+putchar(z[i]);
+}
+}
+return (0);
+}
