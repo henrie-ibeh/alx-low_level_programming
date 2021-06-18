9561d1e930373b7af1b98fa8020f5131b48a4dc8
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-18 03:16:58 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int n;
+int main(void)
+{
+if (n > 0)
+{
+return (n, " is positive\n");
+}
+if (n < 0)
+{
+return (n, " is negative\n");
+}
+else
+{
+return (n, " is zero\n");
+}
+return (0);
+}
