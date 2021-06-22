edb5f3d7ccdcd19119c8e164e7b62d939c38d077
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-22 13:05:20 -0700

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int print_alphabet();
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+int print_alphabet(void)
+{
+int i, len;
+char z[] = "abcdefghijklmnopqrstuvwxyz";
+len = strlen(z);
+for (i =0; i < len; i++)
+{
+putchar(z[i]);
+}
+putchar('\n');
+return (0);
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
