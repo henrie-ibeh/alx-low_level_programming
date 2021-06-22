9b9fd2a28ffc4838203e96de05c06f1a387d63d3
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-22 11:51:04 -0700

    0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int print_alphabet(void)
+{
+int i, len;
+char z[] = "abcdefghijklmnopqrstuvwxyz";
+len = strlen(z);
+for (i =0; i < len; i++)
+{
+putchar(z[i]);
+}
+putchar('\n');
+return (0);
+}
+int print_alphabet_x10(void)
+{
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+print_alphabet();
+return (0);
+}
