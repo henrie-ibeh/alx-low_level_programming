e68b5a779bcdb66e7fd3c463cb8c9143e8b14770
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-22 07:11:06 -0700

    0

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ *main - prints string
+ *Description: prints a string
+ *Return: 0
+ */
+int main(void)
+{
+printf("Holberton\n");
+return (0);
+}
