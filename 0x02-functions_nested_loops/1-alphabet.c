acc6976d63261df9d29b20491485937947fa9b22
henrie-ibeh <ibehhenrie@gmail.com>
2021-06-22 13:03:48 -0700

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-#include <time.h>
-#include "holberton.h"
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int print_alphabet();
